67956a148e8233f1497abacd58e70928cf59bc29
SurajSi789 <surajsinghfirebase7@gmail.com>
2024-03-25 16:56:32 +0530

    added addition class

---
+++ src/main/java/com/demo/DemoMavenProj/AddPerform.java
@@ -0,0 +1,16 @@
+package com.demo.DemoMavenProj;
+
+public class AddPerform {
+
+	public static void main(String[] args) {
+		//addition of two numbers
+		
+		int a = 10;
+		int b = 20;
+		
+		int sum = a + b;
+		
+		System.out.println("Sum of two numbers is: " + sum);
+	}
+
+}
